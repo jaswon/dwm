deeb1749fc4d5b985b6a2fec2de33c61b186d357
jaswon5791 <jasonwon628@gmail.com>
2018-09-12 12:34:27 -0400

    upper limit nmaster

--- dwm.c
+++ dwm.c
@@ -1079,7 +1079,10 @@ grabkeys(void)
 void
 incnmaster(const Arg *arg)
 {
-	selmon->nmaster = MAX(selmon->nmaster + arg->i, 0);
+	int n;
+	Client *c;
+	for (n = 0, c = nexttiled(selmon->clients); c; c = nexttiled(c->next), n++);
+	selmon->nmaster = MIN(MAX(selmon->nmaster + arg->i, 0),n);
 	arrange(selmon);
 }
 

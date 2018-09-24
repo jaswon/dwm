558f42dc758914278b9ccdb6bb6f350b76336dd3
jaswon5791 <jasonwon628@gmail.com>
2018-09-24 01:20:07 -0400

    termite -> kitty

--- config.h
+++ config.h
@@ -59,7 +59,7 @@ static void tagview (const Arg *arg);
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
-static const char *termcmd[]  = { "termite", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char *bltupcmd[]  = { "blt", "+", NULL };
 static const char *bltdncmd[]  = { "blt", "-", NULL };
 static const char *volupcmd[]  = { "vol", "+", NULL };

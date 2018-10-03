e26d975a686fecf18ebc647512db6109ea3ecb1e
jaswon5791 <jasonwon628@gmail.com>
2018-10-02 23:18:32 -0400

    tewi -> curie

--- dwm.c
+++ dwm.c
@@ -714,10 +714,11 @@ drawbar(Monitor *m)
 			urg |= c->tags;
 	}
 	x = 0;
-	w = TEXTW("\u25a0");
 	for (i = 0; i < LENGTH(tags); i++) {
+		const char *tag_symbol = occ & 1 << i ? occupied_tag : unoccupied_tag;
+		w = TEXTW(tag_symbol);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
-		drw_text(drw, x, 0, w, bh, lrpad / 2, occ & 1 << i ? "\u25a0" : "\u25a1", urg & 1 << i);
+		drw_text(drw, x, 0, w, bh, lrpad / 2, tag_symbol, urg & 1 << i);
 		x += w;
 	}
 	w = blw = TEXTW(m->ltsymbol);

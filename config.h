0212367b2a935d7888739fcfa69c7fd5e88636b6
jaswon5791 <jasonwon628@gmail.com>
2019-01-26 17:56:30 -0500

    font and height

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int barsize   = 16;       /* size of bar */
+static const unsigned int barsize   = 20;       /* size of bar */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Wuncon Siji-10", "Input-11" };
+static const char *fonts[]          = { "Wuncon Siji-10", "UW Ttyp0-13" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";

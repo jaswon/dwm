32ef04abe87b78130f51c6b7a59ea1fe5a09e528
jaswon5791 <jasonwon628@gmail.com>
2019-02-21 11:53:58 -0500

    use super for modkey

--- config.h
+++ config.h
@@ -49,7 +49,7 @@ static const Layout layouts[] = {
 };
 
 static void tagview (const Arg *arg);
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
